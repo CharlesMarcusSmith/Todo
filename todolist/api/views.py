1af5325e1298bfe77dc8632eb6b6cb76d111425f
llamaladejam <charliemarcussmith@googlemail.com>
2023-02-17 03:28:07 +0000

    CS: JSON Rest Framework Response: Imports added; Decorator added to apiOverview method; Method adapted to return all future planned urls, which allows anyone working on the API to view to url patterns.

--- todolist/api/views.py
+++ todolist/api/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['GET'])
 def apiOverview(request):
-    return JsonResponse("API BASE POINT", safe=False)
+    # urls we are going to create
+    #By returning this, it allows anyone working on our API to view our url patterns.
+    api_urls = {
+        'List':'/task-list/',
+		'Detail View':'/task-detail/<str:pk>/',
+		'Create':'/task-create/',
+		'Update':'/task-update/<str:pk>/',
+		'Delete':'/task-delete/<str:pk>/',
+    }
+
+    return Response(api_urls)
