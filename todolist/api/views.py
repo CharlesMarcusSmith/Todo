ad059425e4f57c0f77e5c4658809155ca2ef04d9
llamaladejam <charliemarcussmith@googlemail.com>
2023-02-18 03:50:27 +0000

    CS: added some basic responses.

--- todolist/api/views.py
+++ todolist/api/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import TaskSerializer
+
+from .models import Task
 
 # Create your views here.
 @api_view(['GET'])
@@ -17,3 +20,23 @@ def apiOverview(request):
     }
 
     return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.object.all()                       #Querying the Task object, saved as variable
+    serializer = TaskSerializer(task, many=True)    # Set serializer, passing in the tasks variable, and returning many responses
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.object.all()                       #Querying the Task object, saved as variable
+    serializer = TaskSerializer(task, many=True)    # Set serializer, passing in the tasks variable, and returning many responses
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def taskCreate(request):
+    serializer = TaskSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()    
+    return Response(serializer.data)
